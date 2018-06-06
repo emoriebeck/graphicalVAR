f6350c8b7e2dd4890140612b0c5758a03c5ec0f1
Emorie Beck <edbeck@wustl.edu>
2018-06-06 15:32:36 -0500

    tol

--- R/graphicalVAR.R
+++ R/graphicalVAR.R
@@ -10,7 +10,7 @@ computePDC <- function(beta,kappa){
   if (ncol(beta) == nrow(beta)+1){
     beta <- beta[,-1,drop=FALSE]
   }
-  sigma <- solve(kappa)
+  sigma <- solve(kappa, tol = 1e-100)
   t(beta / sqrt(diag(sigma) %o% diag(kappa) + beta^2))
 }
 
